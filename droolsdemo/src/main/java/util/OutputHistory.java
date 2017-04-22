7676f7d8b9efad43be77d9bb635376440394cf80
i007102 <cai.chen@sap.com>
2017-04-22 16:41:42 +0800

    Drools , Global varrarin in rule

---
+++ droolsdemo/src/main/java/util/OutputHistory.java
@@ -0,0 +1,11 @@
+package util;
+
+public class OutputHistory{
+	
+	public void showText(String Text){
+		
+		long time = System.currentTimeMillis();
+		
+		System.out.println("time = " + time + " " +  Text);
+	}
+}
--- droolsdemo/src/test/java/droolsdemo/TestLesson1.java
+++ droolsdemo/src/test/java/droolsdemo/TestLesson1.java
@@ -3,16 +3,18 @@ package droolsdemo;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-import util.KnowledgeSessionHelper;
 
 import org.junit.BeforeClass;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.StatelessKieSession;
+
 import droolscours.Account;
+import util.KnowledgeSessionHelper;
+import util.OutputHistory;
 
 @SuppressWarnings("restriction")
-public class FirstTry {
+public class TestLesson1 {
 	
 	StatelessKieSession sessionStateless = null;
 	KieSession sessionStatefull = null;
@@ -26,7 +28,14 @@ public class FirstTry {
 	
 	@Test
 	public void testFirstOne() {
-		sessionStatefull = KnowledgeSessionHelper.getStateFullKnowledgeSession(kieContainer, "ksession-rules");
+		sessionStatefull = KnowledgeSessionHelper.getStateFullKnowledgeSession(kieContainer, "ksession-rules-lesson1");
+		
+		OutputHistory outputHistory = new OutputHistory();
+		
+		Account TestAccount = new Account();
+		sessionStatefull.setGlobal("showResult", outputHistory);
+		
+		sessionStatefull.insert(TestAccount);
 		sessionStatefull.fireAllRules();
 	}
 
