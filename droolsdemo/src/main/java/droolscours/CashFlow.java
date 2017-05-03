e899cc8d05a7f6a4b163211b34d7ae9bf993a55b
cai.chen <cai.chen@sap.com>
2017-05-03 10:49:43 +0800

    add TestCase in Drools lesson2

--- droolsdemo/src/main/java/droolscours/CashFlow.java
+++ droolsdemo/src/main/java/droolscours/CashFlow.java
@@ -4,59 +4,64 @@ import java.text.DateFormat;
 import java.util.Date;
 
 public class CashFlow {
-	
+
 	public static int CREDIT = 1;
 	public static int DEBIT = 2;
-	
+
 	private Date mvDate;
 	private double amount;
 	private int type;
 	private long accountNo;
-	
-	public Date getMyDate() {
+
+	public Date getMvDate() {
 		return mvDate;
 	}
-	public void setMyDate(Date myDate) {
+
+	public void setMvDate(Date myDate) {
 		this.mvDate = myDate;
 	}
+
 	public double getAmount() {
 		return amount;
 	}
+
 	public void setAmount(double amount) {
 		this.amount = amount;
 	}
+
 	public int getType() {
 		return type;
 	}
+
 	public void setType(int type) {
 		this.type = type;
 	}
+
 	public long getAccountNo() {
 		return accountNo;
 	}
+
 	public void setAccountNo(long accountNo) {
 		this.accountNo = accountNo;
 	}
+
 	@Override
 	public String toString() {
-		StringBuffer buff= new StringBuffer();
-		
+		StringBuffer buff = new StringBuffer();
+
 		buff.append("------CashFlow-----)\n");
 		buff.append("Account no is " + this.getAccountNo() + "\n");
-		
-		if(this.mvDate != null){
-			buff.append("Movement Date= "
-					+ DateFormat.getDateInstance().format(this.mvDate)
-					+ "\n");
-		}else{
-			buff.append("No Movement Date was set"+ "\n");
+
+		if (this.mvDate != null) {
+			buff.append("Movement Date= " + DateFormat.getDateInstance().format(this.mvDate) + "\n");
+		} else {
+			buff.append("No Movement Date was set" + "\n");
 		}
-		
+
 		buff.append("Movenet Amount= " + this.amount + "\n");
 		buff.append("-------CashFlow End --)");
-		
+
 		return buff.toString();
 	}
-	
 
 }
--- droolsdemo/src/test/java/droolsdemo/TestLesson2.java
+++ droolsdemo/src/test/java/droolsdemo/TestLesson2.java
@@ -1,6 +1,7 @@
 package droolsdemo;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -10,10 +11,11 @@ import org.kie.api.runtime.StatelessKieSession;
 
 import droolscours.Account;
 import droolscours.AccountingPeriod;
+import droolscours.CashFlow;
+import util.DateHelper;
 import util.KnowledgeSessionHelper;
 import util.OutputDisplay;
 
-@SuppressWarnings("restriction")
 public class TestLesson2 {
 	StatelessKieSession sessionStateless = null;
 	KieSession sessionStatefull = null;
@@ -50,6 +52,64 @@ public class TestLesson2 {
 		sessionStatefull.insert(period);
 
 		sessionStatefull.fireAllRules();
+	}
+
+	@Test
+	public void testTwoFact() {
+		sessionStatefull = KnowledgeSessionHelper.getStateFulKnowledgeSessionWithCallBack(kieContainer,
+				"ksession-rules-lesson2");
+
+		OutputDisplay outputDisply = new OutputDisplay();
+		sessionStatefull.setGlobal("showResult", outputDisply);
+
+		Account testAccount = new Account();
+		testAccount.setAccountno(1);
+		testAccount.setBalance(0);
+
+		sessionStatefull.insert(testAccount);
+
+		CashFlow cash1 = new CashFlow();
+		cash1.setAccountNo(1);
+		cash1.setAmount(1000);
+		cash1.setType(CashFlow.CREDIT);
+
+		sessionStatefull.insert(cash1);
+		sessionStatefull.fireAllRules();
+		Assert.assertEquals(testAccount.getBalance(), 1000.0, 0);
+
+	}
+
+	@Test
+	public void testTwoFactTwoCashFlowMovement() throws Exception {
+		sessionStatefull = KnowledgeSessionHelper.getStateFulKnowledgeSessionWithCallBack(kieContainer,
+				"ksession-rules-lesson2");
+
+		OutputDisplay outputDisply = new OutputDisplay();
+		sessionStatefull.setGlobal("showResult", outputDisply);
+
+		Account testAccount = new Account();
+		testAccount.setAccountno(1);
+		testAccount.setBalance(0);
+
+		sessionStatefull.insert(testAccount);
+
+		CashFlow cash1 = new CashFlow();
+		cash1.setAccountNo(1);
+		cash1.setAmount(1000);
+		cash1.setMvDate(DateHelper.getDate("2010-01-15"));
+		cash1.setType(CashFlow.CREDIT);
+		sessionStatefull.insert(cash1);
+
+		CashFlow cash2 = new CashFlow();
+		cash2.setAccountNo(2);
+		cash2.setAmount(1000);
+		cash2.setMvDate(DateHelper.getDate("2010-01-15"));
+		cash2.setType(CashFlow.CREDIT);
+		sessionStatefull.insert(cash2);
+
+		sessionStatefull.fireAllRules();
+		Assert.assertEquals(testAccount.getBalance(), 1000.0, 0);
+
 	}
 
 }
